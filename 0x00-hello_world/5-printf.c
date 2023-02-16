9f66808c7a0fad3060d88e88460253940fe16513
malopro <malovagilbert@gmail.com>
2023-02-16 04:21:33 -0800

    added task 5 solution

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * file: 5-printf.c
+ * author: malova
+ * description: prints out a string
+ */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
