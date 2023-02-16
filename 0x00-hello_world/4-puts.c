7164e55472b8d9419e9265aead9bd1000a5c3a06
malopro <malovagilbert@gmail.com>
2023-02-16 03:52:15 -0800

    modified task 4 solution

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
-/**
- * main outputs text to standard output
+/*
+ * File: 4-puts.c
+ * Author: Malova
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ *              to the standard output, followed by a new line.
  */
+
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
