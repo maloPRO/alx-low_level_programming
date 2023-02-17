4d848725383d19580ffc4fa6897dcc32753af92f
malopro <malovagilbert@gmail.com>
2023-02-17 09:39:02 -0800

    add task 9 solution

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+	char comma = ',';
+	char space = ' ';
+
+	while (n < 10)
+	{
+		if (n != 9)
+		{
+			putchar(n + '0');
+			putchar(comma);
+			putchar(space);
+		}
+		else
+		{
+			putchar(n + '0');
+		}
+		n++;
+	}
+
+}
