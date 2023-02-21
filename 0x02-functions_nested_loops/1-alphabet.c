12b5b6e0111c6f35cad744c2d69c85fe5a1626a9
malopro <malovagilbert@gmail.com>
2023-02-21 12:45:51 -0800

    added task 1 solution

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
