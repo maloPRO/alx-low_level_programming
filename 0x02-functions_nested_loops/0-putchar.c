5e9f93fcff74a8e972ba1fb3fc52bab7312a91a1
malopro <malovagilbert@gmail.com>
2023-02-21 09:02:52 -0800

    modified task 0 solution

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,6 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
+	_putchar("_putchar\n");
 	return (0);
 }
