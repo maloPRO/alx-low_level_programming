4b831fee947228116e6cf51b4c7808f44f3154a9
malopro <malovagilbert@gmail.com>
2023-02-21 12:32:17 -0800

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-void _putchar(char *str);
+int _putchar(char c);
 
 /**
  * main - Entry point
@@ -11,6 +11,14 @@ void _putchar(char *str);
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
  *
  * Return: Always 0
  */
-int  _putchar(char c)
+int _putchar(char c)
 {
 	putchar(c);
 	return (0);
