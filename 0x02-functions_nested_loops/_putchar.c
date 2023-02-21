abea1919950975941f37a1be55f8ca9a18e881dc
malopro <malovagilbert@gmail.com>
2023-02-21 12:21:59 -0800

    task 0

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _putchar - priths letters
+ *
+ * @c: letter to  be printed
+ *
+ * Return: Always 0
+ */
+int  _putchar(char c)
+{
+	putchar(c);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,20 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
-/**
- * _putchar - prints word
- *
- * @str: string
- *
- * Return:  Always char
- *
- */
-void _putchar(char *str)
-{
-	printf("%s", str);
-	return;
-}
+void _putchar(char *c);
 
 #endif
-
