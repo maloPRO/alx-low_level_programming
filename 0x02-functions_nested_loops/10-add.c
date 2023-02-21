a7561ac3e57491df02b2041b132947c53cec2805
malopro <malovagilbert@gmail.com>
2023-02-21 14:27:53 -0800

    added task 10 solution

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
-/** 
+/**
  * add - adds values
  *
+ * @int: value
+ *
+ * Return: result
  */
 int add(int, int)
 {
-	return (int +int);
+	int result = int + int;
+
+	return (result);
 }
