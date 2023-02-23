db5b005b363eb8d801e0ebf40e9499bd2d270bae
malopro <malovagilbert@gmail.com>
2023-02-23 13:06:19 -0800

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,35 +8,27 @@ void fizzBuzz(void);
  */
 int main(void)
 {
-	fizzBuzz();
-	printf("\n");
-	return (0);
-}
-
-/**
- * fizzBuzz - prints numbers 0 - 100
- */
-void fizzBuzz(void)
-{
 	int i;
 
 	for (i = 1; i < 100; i++)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz\t");
+			printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz\t");
+			printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\t");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d\t", i);
+			printf("%d ", i);
 		}
 	}
+	printf("\n");
+	return (0);
 }
