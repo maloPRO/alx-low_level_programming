32c4c233307ec324eb21abb4e115ab7e5e797c09
malopro <malovagilbert@gmail.com>
2023-02-23 01:43:58 -0800

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isupper-checks case
+ * Return: 1 if uppercase 0 if lowercase
+ * @c: letter to be checked
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
