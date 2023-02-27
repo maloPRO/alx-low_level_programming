b790d61eb7a3019495889c0bd55c7aab9dc92d4d
malopro <malovagilbert@gmail.com>
2023-02-27 11:29:43 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints out string
+ * @str: string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
