9a43a06c4c7973b22c1d99f494ea4cfd7019b65e
malopro <malovagilbert@gmail.com>
2023-02-27 21:50:48 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string
+ */
+void rev_string(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
