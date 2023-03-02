ff480a61eb733a2ae3f0d90b814b4f9a8bd6545d
malopro <malovagilbert@gmail.com>
2023-03-02 09:47:26 -0800

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: value
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+	s1++;
+	s2++;
+	}
+	return (*s1 - *s2);
+}
+
