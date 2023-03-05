10256aa92bd0c0b4faa920cd819973fc92932de4
malopro <malovagilbert@gmail.com>
2023-03-05 02:50:05 -0800

    task 2

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * reverse_array - reverses array
  * @a: array
  * @n: number of elements
  */
-void reverse_array(int *a, int *n)
+void reverse_array(int *a, int n)
 {
-	int i;
 
-	for (i = n; i <= 0; i--)
-	{
-		_putchar(a[i] + '0');
-	}
 }
 
