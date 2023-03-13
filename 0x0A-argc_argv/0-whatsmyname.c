2e29684b258b25128ea1cb627301a54c2e0dda21
malopro <malovagilbert@gmail.com>
2023-03-13 08:00:37 -0700

    0x0A task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argumen vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
