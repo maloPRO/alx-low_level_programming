6a63587ef5ba48d7e5ee4c5d599a8d56b68b023e
malopro <malovagilbert@gmail.com>
2023-03-19 11:29:06 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+/**
+ * cap_string - capitalizes letters in a string
+ * Return: Capitalized string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	str[0] = toupper(str[0]);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		str[i] == '!' || str[i] == '?' || str[i] == '\\' ||
+		str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+		str[i] == '}' || str[i] == '-')
+		{
+			i++;
+			str[i] = toupper(str[i]);
+		}
+	i++;
+	}
+	return (str);
+}
