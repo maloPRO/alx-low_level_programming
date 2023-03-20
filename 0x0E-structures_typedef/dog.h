ff176eaba1355d080299cb3ce54d3c44aad15920
malopro <malovagilbert@gmail.com>
2023-03-20 08:40:57 -0700

    0x0E Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct for dogs
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
