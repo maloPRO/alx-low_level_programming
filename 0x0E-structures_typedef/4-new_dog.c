2478f94257f563579f7d02c255df089889f3f921
malopro <malovagilbert@gmail.com>
2023-03-20 12:28:24 -0700

    0x0E Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog = creates new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t  *new_doggo;
+	new_doggo = malloc(sizeof(dog_t));
+
+	new_doggo->name = name;
+	new_doggo->age = age;
+	new_doggo->owner = owner;
+
+	return (new_doggo);
+}
