518957d05e19283703e4015e549584b439c66215
malopro <malovagilbert@gmail.com>
2023-03-31 09:35:09 -0700

    0x12 Task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_text - prints text
+ */
+void print_text(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
