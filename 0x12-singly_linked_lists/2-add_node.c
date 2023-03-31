ebd8c43d0495e303b137b686732d6809785ca832
malopro <malovagilbert@gmail.com>
2023-03-31 08:56:03 -0700

    0x12 Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node = malloc(sizeof(sizeof(list_t)));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 	{
@@ -26,6 +26,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
