b1c0a52e75164a06b105a6dc0381ea6dbd90931f
malopro <malovagilbert@gmail.com>
2023-04-04 19:50:05 -0700

    0x13 Task 102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - free nodes
+ * @h: pointer to head
+ * Return: number of nodes
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t count = 0;
+	listint_t *tmp, *current = *h;
+
+	while (current != NULL)
+	{
+		tmp = current;
+		current = current->next;
+		free(tmp);
+		count++;
+
+		if (tmp <= current)
+		{
+			break;
+		}
+	}
+
+	*h = NULL;
+	return (count);
+}
+
