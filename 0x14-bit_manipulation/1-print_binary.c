63d36e06fac906cf10475efc947b50f7e24724a3
malopro <malovagilbert@gmail.com>
2023-04-06 08:17:08 -0700

    0x14 Task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
 * print_binary - prints the binary representation of a number
 * @n: the number to print in binary
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,4 +3,5 @@
 unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
