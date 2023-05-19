9772db29e4d713e33aa664359ef107ea21dc780e
maloPRO <malovagilbert@gmail.com>
2023-05-19 09:22:21 +0200

    Task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,6 +31,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
             current = current->next;
         }
         current->next = new_node;
+        new_node->prev = current;
     }
     return (new_node);
 }
