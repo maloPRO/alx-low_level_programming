2eda9bea19e7c25cba75519112bfe718150f1fa5
maloPRO <malovagilbert@gmail.com>
2023-05-22 15:04:16 +0200

    Task 1

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source_files=$(find . -name "*.c")
+
+object_files=""
+for file in $source_files; do
+    filename=$(basename "$file" .c)
+    gcc -c -fPIC "$file" -o "$filename.o"
+    object_files+=" $filename.o"
+done
+
+gcc -shared $object_files -o liball.so
+
+rm -f *.o
