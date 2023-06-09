c72855e1b074acf4271c7b18f9ddd0253e684e12
malopro <malovagilbert@gmail.com>
2023-06-09 11:45:39 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,21 +3,21 @@
 /**
  * hash_table_create - Creates a hash table
  * @size: size of array
- * Returns: a hash table
+ * Return: a hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* hash_table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	unsigned long int i;
 
-	 if (hash_table == NULL)
-	 {
-		 return (NULL);
-	 }
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
 
-	 hash_table->size = size;
-	 hash_table->array = (hash_node_t**)malloc(size * sizeof(hash_node_t*));
+	hash_table->size = size;
+	hash_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 
 	if (hash_table->array == NULL)
 	{
