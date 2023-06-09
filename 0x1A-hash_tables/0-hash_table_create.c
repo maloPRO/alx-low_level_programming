983b28ab47c549234a66c93b93fd54f26b9573ef
malopro <malovagilbert@gmail.com>
2023-06-09 11:39:16 -0700

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: size of array
+ * Returns: a hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* hash_table = (hash_table_t*)malloc(sizeof(hash_table_t));
+	unsigned long int i;
+
+	 if (hash_table == NULL)
+	 {
+		 return (NULL);
+	 }
+
+	 hash_table->size = size;
+	 hash_table->array = (hash_node_t**)malloc(size * sizeof(hash_node_t*));
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = 0;
+	}
+
+	return (hash_table);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,9 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +32,5 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+hash_table_t *hash_table_create(unsigned long int size);
+#endif
